978c75eb1e3657267ce753b787f7865095515307
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2024-01-05 22:48:46 +0700

    Membuat Interface DestinasiNavigasi

--- app/src/main/java/com/example/projectakhirpam/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/projectakhirpam/navigasi/DestinasiNavigasi.kt
@@ -1,6 +1,8 @@
 package com.example.projectakhirpam.navigasi
 
 interface DestinasiNavigasi {
+    //Nama unik untuk menentukan jalur untuk composable
     val route: String
+    //String resource id yang berisi judul yang akan ditampilkan dilayar halaman
     val titleRes: Int
 }
